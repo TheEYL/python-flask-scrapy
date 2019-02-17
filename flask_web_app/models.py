750f8502e9d7be82c702b484fef7a5a580b4f1fb
hypatiah <danielle.hassid@gmail.com>
2019-02-17 16:28:48 +0100

    create Update and Create pages and routes

--- flask_web_app/models.py
+++ flask_web_app/models.py
@@ -1,8 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
-# from werkzeug import generate_password_hash, check_password_hash
-
 
 db = SQLAlchemy()
+
 class Movies(db.Model):
   __tablename__ = 'movies'
   id = db.Column(db.Integer, primary_key = True, autoincrement = True)
@@ -10,19 +9,9 @@ class Movies(db.Model):
   url = db.Column(db.String)
   image = db.Column(db.String)
   rating = db.Column(db.String)
-  # email = db.Column(db.String(120), unique=True)
-  # pwdhash = db.Column(db.String(54))
 
   def __init__(self, title, url, image, rating):
     self.title = title.title()
     self.url = url.title()
     self.image = image.title()
     self.rating= rating.title()
-    # self.email = email.lower()
-    # self.set_password(password)
-
-  # def set_password(self, password):
-    # self.pwdhash = generate_password_hash(password)
-
-  # def check_password(self, password):
-    # return check_password_hash(self.pwdhash, password)
